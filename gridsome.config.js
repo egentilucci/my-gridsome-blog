8d39345784cacd4886107034b6dff141df0c4d06
Elia Gentilucci <egentilucci@Elias-MacBook-Air.local>
2020-09-15 19:33:46 +0200

    added gridsome plugins to config

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,29 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'Gridsome Blog',
+  siteDescription: 'A simple blog designed with Gridsome',
+  templates: {
+    Post: '/blog/:title',
+  },
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        route: '/blog/:slug'
+      }
+    }
+  ],
+  transformers: {
+    //Add markdown support to all file-system sources
+    remark: {
+      externalLinksTarget: '_blank',
+      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+      plugins: [
+        '@gridsome/remark-prismjs'
+      ]
+    }
+  }
 }

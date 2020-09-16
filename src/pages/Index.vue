04941b0e8a8e31a3fec57425b395cae18db42945
Culdred <culdred@gmail.com>
2020-09-16 21:25:51 +0200

    main page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,64 @@
 <template>
   <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
+    <header class="header">
+      <h1 v-html="$page.metadata.siteName" />
+      <h2 v-html="$page.metadata.siteDescription" />
+    </header>
+    <section class="posts">
+      <PostList
+        v-for="edge in $page.allPost.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
+    </section>
   </Layout>
 </template>
 
 <script>
+import PostList from "@/components/PostList";
 export default {
+  components: {
+    PostList,
+  },
   metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+    title: "A simple blog",
+  },
+};
 </script>
 
+<page-query>
+  query {
+    metadata {
+      siteName
+      siteDescription
+    }
+    allPost {
+      totalCount
+      edges {
+        node {
+          id
+          title
+          timeToRead
+          description
+          date (format: "D MMMM YYYY")
+          path
+        }
+      }
+
+    }
+  }
+</page-query>
+
 <style>
-.home-links a {
-  margin-right: 1rem;
+.header {
+  font-family: "Stylish";
+  font-size: 35px;
+  text-align: center;
+  line-height: 1.4em;
+  padding: 0.7em;
+}
+.header h2 {
+  font-weight: 200;
+  font-size: 35px;
 }
 </style>

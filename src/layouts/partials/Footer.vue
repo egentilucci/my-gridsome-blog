a508872dbf5d2ac340742443b986ee6e51bc57cb
Culdred <culdred@gmail.com>
2020-09-16 21:25:26 +0200

    footer for default page

---
+++ src/layouts/partials/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="footer">
+      <p>
+        Built with
+        <a class="link" href="//gridsome.org">Gridsome</a>
+        & Made with ❤️ by
+        <a class="link" href="//twitter.com/lauragift_">Gift Egwuenu</a>
+      </p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+  .footer {
+    padding-top: calc(30em / 4);
+    text-align: center;
+    font-size: 1em;
+  }
+</style>

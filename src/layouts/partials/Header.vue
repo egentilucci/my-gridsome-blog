e03253bd625f1ed7e84e8c8d7c87665579fb1394
Culdred <culdred@gmail.com>
2020-09-16 22:40:24 +0200

    added fixed navbar [WIP]

--- src/layouts/partials/Header.vue
+++ src/layouts/partials/Header.vue
@@ -2,7 +2,8 @@
   <div>
     <header class="layout-header">
       <nav class="nav">
-        <ToggleTheme />
+        <div class="logo">LOGO</div>
+        <div class="toggle-theme"><ToggleTheme /></div>
       </nav>
     </header>
   </div>
@@ -18,13 +19,28 @@ export default {
 </script>
 
 <style>
-.layout-header {
-  width: 100%;
-  margin: 0px auto;
-  margin-top: 30px;
-}
-.nav {
-  display: flex;
-  justify-content: flex-end;
-}
+  .layout-header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    margin: auto;
+    width: 100%;
+  }
+  .nav {
+    justify-content: space-between;
+    border: 1px solid rgb(111,41,97);
+    padding: 5px;
+    display: flex;
+    align-items: center;
+  }
+  .logo {
+    padding: 5px;
+    border: 1px solid rgba(111,41,97,.5);
+    border-radius: .5em;
+  }
+  .toggle-theme {
+    padding: 5px;
+    border: 1px solid rgba(111,41,97,.5);
+    border-radius: .5em;
+  }
 </style>
